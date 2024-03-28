fdcbe7b2e4bd45d19755f206428e6502e8ec3fd9
yetniek <51157811+yetniek@users.noreply.github.com>
2024-03-29 01:21:09 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240329/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+jewelrys = [tuple(map(int, input().split())) for _ in range(n)]
+answer = 0
+
+jewelrys.sort(key=lambda x:-x[1]/x[0])
+
+for w, v in jewelry:
+    if m >= w: # 현재 보석을 다 담을 수 있다면 그대로 담아줌
+        m -= w
+        answer += v 
+    else: # 부분만 담을 수 있다면 비율에 맞춰 담아둔 뒤 종료
+        answer += m / w*v
+        break
+print(f"{answer:.3f}")
