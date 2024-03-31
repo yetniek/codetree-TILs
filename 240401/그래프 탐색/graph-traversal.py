3ebadd29b79278f185ed550b75a93f5da5ff055f
yetniek <51157811+yetniek@users.noreply.github.com>
2024-04-01 02:40:57 +0900

    Title: 그래프 탐색, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240401/그래프 탐색/graph-traversal.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+v_cnt = 0 
+
+def dfs(v):
+    global v_cnt
+
+    for curr_v in graph[v]:
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            v_cnt += 1
+            def(curr_v)
+
+for i in range(m):
+    v1, v2 = map(int, input().split())
+
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+visited[1] = True
+dfs(1)
+
+print(v_cnt)
