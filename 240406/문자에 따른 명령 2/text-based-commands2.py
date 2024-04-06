76e1a84f68b4d8a7426eaf1ec895a55c0f0da243
yetniek <51157811+yetniek@users.noreply.github.com>
2024-04-06 12:44:56 +0900

    Title: 문자에 따른 명령 2, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240406/문자에 따른 명령 2/text-based-commands2.py
+++ 240406/문자에 따른 명령 2/text-based-commands2.py
@@ -8,17 +8,16 @@ dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
 dir_num = 3
 
 for i in directions:
-    if i == 'L': # 반시계 
+    if i == 'L': # 반시계 - 방향만 이동
         dir_num = (dir_num - 1 + 4) % 4 
+    elif i == 'R': # 시계 - 방향만 이동
+        dir_num = (dir_num + 1) % 4 
+    else: # 직진
         x += dxs[dir_num]
         y += dys[dir_num] 
-    elif i == 'R': # 시계
-        dir_num = (dir_num + 1) % 4
-        x += dxs[dir_num]
-        y += dys[dir_num]
 
 print(x, y)
-'''
+''' 
 시계방향 90도 회전은 dx, dy를 + 1 해주면 됨(= dir_num + 1)
 그런데 3일 경우 -> 0이 되어야함
 따라서 ( dir_num + 1 ) % 4 
