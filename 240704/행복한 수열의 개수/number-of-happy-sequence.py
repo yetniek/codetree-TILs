100c7af049981ce2abc5cd84b3247f2c97c2f34f
yetniek <51157811+yetniek@users.noreply.github.com>
2024-07-04 11:05:37 +0900

    Title: 행복한 수열의 개수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240704/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,37 +1,71 @@
+def count_happy_sequences(grid, n, m):
+    def is_happy_sequence(seq, m):
+        count = 1
+        for i in range(1, len(seq)):
+            if seq[i] == seq[i-1]:
+                count += 1
+            else:
+                count = 1
+            if count >= m:
+                return True
+        return False
+    
+    happy_count = 0
+    
+    # Check each row
+    for row in grid:
+        if is_happy_sequence(row, m):
+            happy_count += 1
+    
+    # Check each column
+    for col in range(n):
+        column_seq = [grid[row][col] for row in range(n)]
+        if is_happy_sequence(column_seq, m):
+            happy_count += 1
+    
+    return happy_count
+
+
+
 n, m = map(int, input().split()) 
 grid = [list(map(int, input().split())) for _ in range(n)] 
-seq = [0 for _ in range(n)] 
 
-def is_happy_seq():
-    consecutive_cnt, max_ccnt = 1, 1
+print(count_happy_sequences(grid, n, m))  # Output: 6
 
-    for i in range(1, n):
-        if seq[i-1] == seq[i]:
-            consecutive_cnt += 1
-        else:
-            consecutive_cnt = 1
+# n, m = map(int, input().split()) 
+# grid = [list(map(int, input().split())) for _ in range(n)] 
+# seq = [0 for _ in range(n)] 
 
-        max_ccnt = max(max_ccnt, consecutive_cnt)
+# def is_happy_seq():
+#     consecutive_cnt, max_ccnt = 1, 1
 
-    return max_ccnt >= m
+#     for i in range(1, n):
+#         if seq[i-1] == seq[i]:
+#             consecutive_cnt += 1
+#         else:
+#             consecutive_cnt = 1
 
-num_happy = 0
+#         max_ccnt = max(max_ccnt, consecutive_cnt)
 
-for i in range(n):
-    seq = grid[i][:]
+#     return max_ccnt >= m
 
-    if is_happy_seq():
-        num_happy += 1
+# num_happy = 0
 
-# vertical_lst = list(zip(*grid)) 
 # for i in range(n):
-#     seq = vertical_lst[i][:]
+#     seq = grid[i][:]
 
-for j in range(n):
-    for i in range(n):
-        seq[i] = grid[i][j]
+#     if is_happy_seq():
+#         num_happy += 1
 
-    if is_happy_seq():
-            num_happy += 1
+# # vertical_lst = list(zip(*grid)) 
+# # for i in range(n):
+# #     seq = vertical_lst[i][:]
 
-print(num_happy)
+# for j in range(n):
+#     for i in range(n):
+#         seq[i] = grid[i][j]
+
+#     if is_happy_seq():
+#             num_happy += 1
+
+# print(num_happy)
