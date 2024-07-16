940638ab0ed533042fc905e6618c0a3a290d28be
YJLee <51157811+yetniek@users.noreply.github.com>
2024-07-16 17:00:58 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,15 @@
+order = input()
+
+dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+initx, inity = 0, 0
+num = 3
+for i in order:
+    if i == 'L':
+        num = (num - 1 + 4) % 4
+        dx, dy = dirs[num] 
+    elif i == 'R':
+        num = (num + 1) % 4
+        dx, dy = dirs[num]
+    elif i == 'F':  
+        nx, ny = initx + dx, inity + dy 
+print(nx, ny)
