08755c027897a4b8cd86cef6b32c717b0c8ce421
YJLee <51157811+yetniek@users.noreply.github.com>
2024-07-16 17:07:42 +0900

    Title: 문자에 따른 명령 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240716/문자에 따른 명령 2/text-based-commands2.py
+++ 240716/문자에 따른 명령 2/text-based-commands2.py
@@ -6,11 +6,10 @@ num = 3
 for i in order:
     if i == 'L':
         num = (num - 1 + 4) % 4
-        dx, dy = dirs[num] 
     elif i == 'R':
         num = (num + 1) % 4
-        dx, dy = dirs[num]
     else: 
+        dx, dy = dirs[num]
         initx, inity = initx + dx, inity + dy 
  
 print(initx, inity)
